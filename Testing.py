1c7c0d3ae36d2b9f4088eea82aa9ea7ac4320a40
morhale67 <87977630+morhale67@users.noreply.github.com>
2023-08-17 08:48:33 +0300

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -5,26 +5,28 @@ import wandb
 from LogFunctions import print_and_log_message
 import math
 from OutputHandler import save_outputs
+import numpy as np
 
 
-def test_net(epoch, model, loader, params, device, log_path, folder_path, save_img=False):
+def test_net(epoch, model, loader, device, log_path, folder_path, batch_size, z_dim, img_dim, cr, epochs,
+             save_img=False):
     model.eval()
     model.to(device)
     cumu_loss = 0
+
     for batch_index, sim_bucket_tensor in enumerate(loader):
         sim_object, _ = sim_bucket_tensor
         sim_object.to(device)
-        noise = torch.randn(params['batch_size'], params['z_dim'], requires_grad=True).to(device)
+        noise = torch.randn(batch_size, z_dim, requires_grad=True).to(device)
         sim_diffuser = model(noise)
 
-        sim_diffuser_reshaped = sim_diffuser.reshape(params['batch_size'], math.floor(params['img_dim'] / params['cr']),
-                                             params['img_dim'])
+        sim_diffuser_reshaped = sim_diffuser.reshape(batch_size, math.floor(img_dim / cr), img_dim)
 
-        sim_object = sim_object.view(-1, 1, params['img_dim']).to(device)
+        sim_object = sim_object.view(-1, 1, img_dim).to(device)
         sim_object = sim_object.transpose(1, 2)
         sim_bucket = torch.matmul(sim_diffuser_reshaped, sim_object)
         sim_bucket = torch.transpose(sim_bucket, 1, 2)
-        reconstruct_imgs_batch = breg_rec(sim_diffuser_reshaped, sim_bucket, params['batch_size'])
+        reconstruct_imgs_batch = breg_rec(sim_diffuser_reshaped, sim_bucket, batch_size)
 
         reconstruct_imgs_batch = reconstruct_imgs_batch.to(device)
         sim_object = torch.squeeze(sim_object)
@@ -32,19 +34,22 @@ def test_net(epoch, model, loader, params, device, log_path, folder_path, save_i
         loss = criterion(reconstruct_imgs_batch, sim_object)
         cumu_loss += loss.item()
 
-    avg_loss = cumu_loss / len(loader)
+    test_loss = cumu_loss / len(loader)
     try:
+        pic_width = int(math.sqrt(img_dim))
+        image_reconstructions = [wandb.Image(i.reshape(pic_width, pic_width)) for i in reconstruct_imgs_batch]
+        sim_object_images = [wandb.Image(i.reshape(pic_width, pic_width)) for i in sim_object]
+
         wandb.log({'sim_diffuser': [wandb.Image(i) for i in sim_diffuser_reshaped]})
-        wandb.log({'image reconstructions': [wandb.Image(i.reshape(28, 28)) for i in reconstruct_imgs_batch]})
-        wandb.log({'sim_object images': [wandb.Image(i.reshape(28, 28)) for i in sim_object]})
-        wandb.log({'val_loss': loss})
-        print(f"epoch [{epoch} / {params['epochs']}] \ "
-                    f"genValLoss: {loss:.4f}")
+        wandb.log({'image reconstructions': image_reconstructions})
+        wandb.log({'test original images': sim_object_images})
+        wandb.log({'Test_loss': test_loss})
+
+        print(f"epoch [{epoch} / {epochs}] \ "
+              f"genValLoss: {test_loss:.4f}")
     except:
-        print_and_log_message('Test Loss: {:.6f}\n'.format(avg_loss), log_path)
+        print_and_log_message('Test Loss: {:.6f}\n'.format(test_loss), log_path)
         if save_img:
-            save_outputs(reconstruct_imgs_batch, sim_object, int(math.sqrt(params['img_dim'])), folder_path, 'test_images')
+            save_outputs(reconstruct_imgs_batch, sim_object, int(math.sqrt(img_dim)), folder_path, 'test_images')
 
-    return avg_loss
-
-
+    return test_loss
