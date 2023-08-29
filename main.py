cae53a330262f3e166d513ba05b73194de3768c0
morhale67 <87977630+morhale67@users.noreply.github.com>
2023-08-29 17:21:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import torch
 import wandb
-from Training import train
-from Training import train_local
+from main_training import train
+from main_training import train_local
 from Params import get_run_parameters
 from LogFunctions import print_and_log_message
 from LogFunctions import print_run_info_to_log
 from OutputHandler import make_folder
 
-# wandb.login(key='')
+# wandb.login(key='8aec627a04644fcae0f7f72d71bb7c0baa593ac6')
 #
-# sweep_id = ''
+# sweep_id = '9pejfq8j'
 # wandb.agent(sweep_id, train, project='Optimal Diffuser', count=30)
 
 
@@ -20,4 +20,5 @@ p = get_run_parameters()
 folder_path = make_folder('GEN', p)
 log_path = print_run_info_to_log(p, folder_path)
 print_and_log_message(f'learning rate: {p["lr"]}', log_path)
-train_local(p, log_path, folder_path, Medical=True)
+train_local(p, log_path, folder_path, Medical=False)
+
