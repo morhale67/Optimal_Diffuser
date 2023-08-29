aa86a2a464d1a41468324100b883b7d314e1a57f
morhale67 <87977630+morhale67@users.noreply.github.com>
2023-08-29 17:19:53 +0300

    Update Params.py

--- Params.py
+++ Params.py
@@ -2,15 +2,15 @@ import math
 
 
 def get_run_parameters():
-    p = {'cr': 2,
+    p = {'cr': 1,
          'batch_size': 2,
          'lr': 0.001,
          'epochs': 4,
-         'n_fc': 3,
-         'num_workers': 1,
+         'num_workers': 4,
          'z_dim': 100,
-         'pic_width': 64,
-         'optimizer': 'adam'}
+         'pic_width': 32,
+         'optimizer': 'adam',
+         'ac_stride': 7}
     p['img_dim'] = p['pic_width']*p['pic_width']
     p['n_masks'] = math.floor(p['img_dim'] / p['cr'])
 
