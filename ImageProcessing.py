b8000e9926ff5745b7b58288d02452bd99086dd1
Mor Hale <morhale67@gmail.com>
2023-11-04 15:21:26 +0200

    split images to folders

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -7,6 +7,7 @@ import os
 import bm3d
 from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral, denoise_wavelet)
 from skimage import io, img_as_float
+from PIL import Image
 
 
 def denoise_image(img_new_width=64):
@@ -34,6 +35,99 @@ def denoise_image(img_new_width=64):
         # plt.title(f"denoised after resiezed {i} ")
         # plt.savefig(results_dir + "/" + f"denoised_{i}_resized_{img_new_width}_{img_new_width}.png")
         # plt.show()
+
+
+def eli_resize(scale_factor, img_path, results_dir):
+    input_image = Image.open(img_path).convert("L")
+    width, height = input_image.size
+    new_width = width // scale_factor
+    new_height = height // scale_factor
+    resized_img = Image.new("L", (new_width, new_height))
+
+    for x in range(new_width):
+        for y in range(new_height):
+            # Calculate the corresponding coordinates in the original image
+            x1 = x * scale_factor
+            y1 = y * scale_factor
+
+            # Initialize variables for calculating the average intensity
+            total_intensity = 0
+
+            # Iterate over the pixels in the original image to calculate the average intensity
+            for i in range(scale_factor):
+                for j in range(scale_factor):
+                    intensity = input_image.getpixel((x1 + i, y1 + j))
+                    total_intensity += intensity
+
+            average_intensity = total_intensity // (scale_factor * scale_factor)
+            resized_img.putpixel((x, y), average_intensity)
+
+    resized_img.save(os.path.join(results_dir, "eli_resize.png"))
+    input_image.close()
+    return resized_img
+
+
+def cv2_resize(img_width, img_length, img_path, results_dir):
+    org_img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    org_img = np.array(org_img)
+
+    plt.imshow(org_img, cmap='gray')
+    plt.title(f"original_image")
+    plt.savefig(os.path.join(results_dir, "original_image.png"))
+
+    resized_img = cv2.resize(org_img, (img_width, img_length), interpolation=cv2.INTER_NEAREST)
+    plt.imshow(resized_img, cmap='gray')
+    plt.title(f"INTER_NEAREST")
+    plt.savefig(os.path.join(results_dir, "INTER_NEAREST.png"))
+
+    resized_img = cv2.resize(org_img, (img_width, img_length), interpolation=cv2.INTER_AREA)
+    plt.imshow(resized_img, cmap='gray')
+    plt.title(f"INTER_AREA")
+    plt.savefig(os.path.join(results_dir, "INTER_AREA.png"))
+
+    resized_img = cv2.resize(org_img, (img_width, img_length), interpolation=cv2.INTER_LINEAR)
+    plt.imshow(resized_img, cmap='gray')
+    plt.title(f"INTER_LINEAR")
+    plt.savefig(os.path.join(results_dir, "INTER_LINEAR.png"))
+
+    resized_img = cv2.resize(org_img, (img_width, img_length), interpolation=cv2.INTER_CUBIC)
+    plt.imshow(resized_img, cmap='gray')
+    plt.title(f"INTER_CUBIC")
+    plt.savefig(os.path.join(results_dir, "INTER_CUBIC.png"))
+
+    resized_img = cv2.resize(org_img, (img_width, img_length), interpolation=cv2.INTER_LANCZOS4)
+    plt.imshow(resized_img, cmap='gray')
+    plt.title(f"INTER_LANCZOS4")
+    plt.savefig(os.path.join(results_dir, "INTER_LANCZOS4.png"))
+
+    return resized_img
+
+
+def transforms_resize(img_width, img_length, img_path, results_dir):
+    pass
+
+
+def pillow_resize(img_width, img_path, results_dir):
+    image = Image.open(img_path)
+    resized_img = image.resize((img_width, img_width))
+    resized_img.save(os.path.join(results_dir, "pillow_resize.png"))
+    return resized_img
+
+
+def try_diff_resize(img_width, img_length, img_path, results_dir, algorithms='all'):
+    if algorithms == 'all':
+        algorithms = ['eli', 'cv2'] #, 'pillow', 'transforms']
+    for alg in algorithms:
+        if alg == 'eli':
+            resized_img = eli_resize(4 , img_path, results_dir)
+        if alg == 'cv2':
+            resized_img = cv2_resize(img_width, img_length, img_path, results_dir)
+        if alg == 'pillow':
+            resized_img = pillow_resize(img_width, img_length, img_path, results_dir)
+        if alg == 'transforms':
+            resized_img = transforms_resize(img_width, img_length, img_path, results_dir)
+    return resized_img
+
 
 
 def try_diff_denoising(noisy_img, results_dir, algorithms='all'):
@@ -104,8 +198,13 @@ def folder_image_split(folder_path, num_rows, num_cols, result_folder):
 
 
 if __name__ == '__main__':
-    folder_path = 'data\selected_images\mock_class'
-    result_folder = 'data\images_for_sew\mock_class'
-    folder_image_split(folder_path, 4, 4, result_folder)
+    img_path = r'C:\Users\user\Desktop\Projects\research\com_medical_diffuser\data\selected_images\mock_class' \
+               r'\chunk_middle_part_0060_0798373378_01_WRI-R2_F016.png '
+    # img_path = r'C:\Users\user\Desktop\Projects\research\com_medical_diffuser\data\selected_images\mock_class' \
+    #            r'\image7_original.png '
+    result_dir = 'temp/resize_exp'
+    img_width = 64
+    img_length = 64
+    try_diff_resize(img_width, img_length, img_path, result_dir, algorithms=['eli', 'cv2'])
 
 
