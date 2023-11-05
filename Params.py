f902e1ff5c2616aac527f11de19ca916cd417c23
morhale67 <morhale67@gmail.com>
2023-11-05 14:08:06 +0000

    results after order output, cr=4, cifar

--- Params.py
+++ Params.py
@@ -5,7 +5,7 @@ def get_run_parameters():
     p = {'data_medical': 'data_DSI/GCP_data',
          'data_name': 'cifar',
          'n_samples': 4,
-         'cr': 1,
+         'cr': 4,
          'batch_size': 2,
          'lr': 0.01,
          'epochs': 50,
