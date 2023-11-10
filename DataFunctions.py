174862655ab1cce70f104a5de21f47fb51d8bbda
morhale67 <morhale67@gmail.com>
2023-11-10 09:01:56 +0000

    try no shuffle in split train test 2

--- DataFunctions.py
+++ DataFunctions.py
@@ -41,8 +41,8 @@ def create_loader_from_data_set(data_set, n_samples, batch_size, num_workers, te
     selected_indices = random.sample(indices, n_samples)
 
     train_indices, test_indices = train_test_split(selected_indices, test_size=test_size, random_state=42, shuffle=False)
-    train_indices = adjust_list_length_same_bs(train_indices, batch_size)
-    test_indices = adjust_list_length_same_bs(test_indices, batch_size)
+#    train_indices = adjust_list_length_same_bs(train_indices, batch_size)
+#    test_indices = adjust_list_length_same_bs(test_indices, batch_size)
 
     train_sampler = SubsetRandomSampler(train_indices)
     test_sampler = SubsetRandomSampler(test_indices)
