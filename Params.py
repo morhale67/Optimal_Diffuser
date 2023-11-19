4586acbd35e0163edfafd59ed608b39b3395afe0
morhale67 <morhale67@gmail.com>
2023-11-19 16:03:04 +0000

    order cr 10

--- Params.py
+++ Params.py
@@ -9,7 +9,7 @@ def get_run_parameters():
          'learn_vec_lr': True,
          'pic_width': 32,
          'n_samples': 100,
-         'cr': 3,
+         'cr': 10,
          'batch_size': 2,
          'lr': 0.001,
          'epochs': 5,
